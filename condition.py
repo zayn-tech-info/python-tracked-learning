4dd9f32f5ee08dfac903a9c13d0156086732bb92
zayn-tech-info <zayntech.info@gmail.com>
2025-08-27 18:05:45 +0100

    Add condition checking and welcome message to new Python scripts

---
+++ condition.py
@@ -0,0 +1,14 @@
+will_be_taken_down = True
+name = "zayn"
+
+
+""" if will_be_taken_down == True and name == "zayn": 
+    print("")
+    print('Not today kid !')
+    print("")
+else: 
+    print("")
+    print("Yes, you can take anyone down but not zayn !!!")
+    print("") """
+
+print("not today kid !") if will_be_taken_down == True and name == "zayn" else print("Yes, you can take anyone down but not zayn")
---
+++ welcome.py
@@ -0,0 +1,8 @@
+line1 = "*******************"
+line2 = "*                 *"
+line3=  "*     WELCOME     *"
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
