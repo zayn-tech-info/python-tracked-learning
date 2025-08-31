3dadf6c9d3ec4f7daf3eeb2addbcd6e52a12b5ae
zayn-tech-info <zayntech.info@gmail.com>
2025-08-31 02:56:05 +0100

    Updated rock paper sicssors game

--- rps.py
+++ rps.py
@@ -1,42 +1,53 @@
 import sys
 from enum import Enum
 import random
-class RPS(Enum):
-    rock = 1
-    paper = 2
-    scissors = 3
+ 
+game_count = 0
+def play_rps(): 
+    class RPS(Enum):
+        rock = 1
+        paper = 2
+        scissors = 3
 
-playagain = True
-
-while playagain:
-    print("\nEnter...")
-    print("1 for Rock\n2 for Paper\n3 for Scissors")
-    userChoice = int(input("\nEnter a number to start playing: \n"))
+    print("\nEnter...\n1 for Rock\n2 for Paper\n3 for Scissors")
+    playerChoice = int(input("\nEnter a number to start playing: \n"))
+    if playerChoice not in [1, 2, 3] :
+        print("Please enter a number between 1 to 3")
+        play_rps()
 
     computerChoice = random.choice([1, 2, 3])
-    print("You chose " + RPS(userChoice).name + ".")
+    print("You chose " + RPS(playerChoice).name + ".")
     print("Computer chose " + RPS(computerChoice).name + ".")
 
-    if userChoice > 3 or userChoice < 1:
-        print("Please enter a number between 1 to 3")
-        sys.exit()
-
-    if userChoice == 1 and computerChoice == 2:
-        print("\nYahh, you won!")
-    elif userChoice == 3 and computerChoice == 2:
-        print("\nYahh, You won")
-    elif userChoice == 1 and computerChoice == 3:
-        print("\nyahh, You won")
-    elif userChoice ==  computerChoice:
-        print("\nTie game, Try again")
-    else:
-        print("\nComputer wins!")
+    def handle_winner(player, computer): 
+        if  player == 1 and computer == 2:
+            return "\nYahh, you won!"
+        elif  player == 3 and computer == 2:
+            return "\nYahh, You won"
+        elif  player == 1 and computer == 3:
+            return "\nyahh, You won"
+        elif  player ==  computer:
+            return "\nTie game, Try again"
+        else:
+            return "\nComputer wins!"
     
-    playagain = input('Enter "Y" to play again\n"Q" to quit\n\n')
+    game_result = handle_winner(playerChoice, computerChoice)
+    print(game_result)
+    global game_count
+    game_count += 1
+    print("\nGame result: " + str(game_count))
+
+    print("\nPlay again?")
+    while True:
+        playagain = input('\nEnter "Y" to play again\n"Q" to quit\n\n')
+        if playagain.lower() not in ["y", "q"]:
+            continue
+        else:
+            break
 
     if playagain.lower() == "y":
-        continue
+       return play_rps()
     else:
         print("Thank you for playing")
-        break
-sys.exit("Bye !")
+        sys.exit("Bye !")
+play_rps()
